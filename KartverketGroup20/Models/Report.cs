f4d447b8822e26e01102936ca86f4024e4e5c868
JonasLovik <142986338+JonasLovik@users.noreply.github.com>
2024-11-14 10:20:36 +0100

    før db

--- KartverketGroup20/Models/Report.cs
+++ KartverketGroup20/Models/Report.cs
@@ -7,12 +7,15 @@ namespace KartverketGroup20.Models
     {
         
         public int Id { get; set; }
-        public string? Title { get; set; } = String.Empty;
-        public string? MapType { get; set; }
-        public string? Description { get; set; } = String.Empty;
+        //[ForeignKey("AppUser")]
+        //public int UserId { get; set; }
         public string? GeoJson { get; set; }
-        public DateTime? ReportTime { get; set; } = DateTime.Now;
-        public int UserId { get; set; }
+        public string? Description { get; set; }
+        //public string? MapType { get; set; }
+        //public string? Status { get; set; }
+        //[DataType(DataType.Time)]
+        //public DateTime ReportTime { get; set; }
+        
 
     }
 }
